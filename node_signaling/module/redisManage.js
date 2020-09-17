9ed743966a5b6e2f7e261391f69d92a8d3c6004a
thanhdev449 <thanh.nguyen@vinasaver.vn>
2020-09-17 16:31:34 +0700

    update code

--- node_signaling/module/redisManage.js
+++ node_signaling/module/redisManage.js
@@ -10,68 +10,99 @@ function initializeRedis(ip, port, password) {
   _rd = new RedisClustr({
     servers: [
       {
-        host: '192.164.68.77',
+        host: '192.168.64.77',
         port: 6001,
         db: '0',
-        password: "root"
       },
-      {
-        host: '192.168.64.77',
-        port: 6002,
-        db: '0',
-        password: "root"
-      },
-      {
-        host: '192.168.64.77',
-        port: 6003,
-        db: '0',
-        password: "root"
-      },
-      {
-        host: '192.168.64.77',
-        port: 6004,
-        db: '0',
-        password: "root"
-      },
-      {
-        host: '192.168.64.77',
-        port: 6005,
-        db: '0',
-        password: "root"
-      },
-      {
-        host: '192.168.64.77',
-        port: 6006,
-        db: '0',
-        password: "root"
-      },
+      // {
+      //   host: '172.18.0.1',
+      //   port: 6002,
+      //   db: '0',
+      //   password: ""
+      // },
+      // {
+      //   host: '172.18.0.1',
+      //   port: 6003,
+      //   db: '0',
+      //   password: ""
+      // },
+      // {
+      //   host: '172.18.0.1',
+      //   port: 6004,
+      //   db: '0',
+      //   password: ""
+      // },
+      // {
+      //   host: '172.18.0.1',
+      //   port: 6005,
+      //   db: '0',
+      //   password: ""
+      // },
+      // {
+      //   host: '172.18.0.1',
+      //   port: 6006,
+      //   db: '0',
+      //   password: ""
+      // },
     ],
-    slotInterval: 1000, 
-    maxQueueLength: 100, 
-    queueShift: false,
-    wait: 5000,
+    // slotInterval: 1000, 
+    // maxQueueLength: 100, 
+    // queueShift: false,
+    // wait: 5000,
     slaves: 'share',
     createClient: function(port,host,options) {
-      // this is the default behaviour
       console.log(port,host);
       //return require('redis').createClient(port, host, options);
       //_client = require('redis').createClient("redis://" + "" + ":" + "" + "@" +  host + ":" + port,options);
-      // _client = redis.createClient(port, host, options);
+      return redis.createClient(port, "192.168.64.77", options);
       //return redis.createClient("redis://" + "" + ":" + "" + "@" +  host + ":" + port,options);
-      return redis.createClient("redis://" + "" + ":" + "" + "@" +  "192.168.64.77" + ":" + "6002");
+      //return redis.createClient("redis://" + "" + ":" + "" + "@" +  "172.18.0.1" + ":" + "6003");
     }
   });
 }
 
+// function initializeRedis(ip, port, password) {
+//   _cluster = new redis2.Cluster([
+//       {
+//         host: '192.168.64.77',
+//         port: 6001,
+//         db: '0',
+//         password : "root"
+//       },
+//       {
+//         host: '192.168.64.77',
+//         port: 6002,
+//         db: '0',
+//         password : "root"
+//       },
+//       {
+//         host: '192.168.64.77',
+//         port: 6003,
+//         db: '0',
+//         password : "root"
+//       },
+//       {
+//         host: '192.168.64.77',
+//         port: 6004,
+//         db: '0',
+//       },
+//       {
+//         host: '192.168.64.77',
+//         port: 6005,
+//         db: '0',
+//       },
+//       {
+//         host: '192.168.64.77',
+//         port: 6006,
+//         db: '0',
+//       },
+//   ])
+// }
+
 function setSimple(key,value){
-  //_cluster.set(key,value)
-  //console.log(_rd.connections['172.19.0.2:6002'])
-  _rd.set(key,value,(err,res) =>{
-    if (err) {
-      console.log(err)
-    }else{
-      console.log(res)
-    }
+  _rd.set(key,value,function(err,res){
+    if(err) console.log(err);
+    console.log(res);
   });
 }
 
